45cc5112ed36a6a8b16c8dcfa3da979cf3b515d6
Mazed1413 <mamazed1314@gmail.com>
2024-03-04 15:21:18 +0600

    update script

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const allCardContainer = document.getElementById("all-card-container");
-const readBtn = document.getElementById("read-count");
-let count = 0;
-
 // ----------------------------------------------discuss section function-----------------------------
 const loadData = async () => {
+  const allCardContainer = document.getElementById("all-card-container");
+  let count = 0;
+
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/retro-forum/posts"
+    `https://openapi.programming-hero.com/api/retro-forum/posts`
   );
   const data = await res.json();
-
   data.posts.forEach((item) => {
     // console.log(item);
     const newCard = document.createElement("div");
@@ -18,78 +16,78 @@ const loadData = async () => {
     }
 
     newCard.innerHTML = `<div
-              class="w-full flex gap-4 p-6 bg-gray-100 border border-gray-300 rounded-2xl shadow"
-            >
-              <div class="">
-                <div class="indicator">
-                    ${indicator}
-                  <div
-                    class="grid w-12 h-12 bg-white place-items-center"
-                  ><img src="${item.image}" class="rounded-lg" alt="" /></div>
-                </div>
-              </div>
-              <div class="w-11/12">
-                <div class="flex gap-4">
-                  <p>#${item.category}</p>
-                  <p>author: <span>${item.author.name}</span></p>
-                </div>
-                <a href="#">
-                  <h5 class="mb-2 text-2xl font-bold tracking-tight">
-                    ${item.title}
-                  </h5>
-                </a>
-                <p
-                  class="mb-3 py-2 font-normal text-gray-600 border-b-2 border-gray-300 border-dotted"
-                >
-                  ${item.description}
-                </p>
+                          class="w-full flex gap-4 p-6 bg-gray-100 border border-gray-300 rounded-2xl shadow-lg"
+                        >
+                          <div class="">
+                            <div class="indicator">
+                                ${indicator}
+                              <div
+                                class="grid w-12 h-12 bg-white place-items-center"
+                              ><img src="${item.image}" class="rounded-lg" alt="" /></div>
+                            </div>
+                          </div>
+                          <div class="w-11/12">
+                            <div class="flex gap-4">
+                              <p>#${item.category}</p>
+                              <p>author: <span>${item.author.name}</span></p>
+                            </div>
+                            <a href="#">
+                              <h5 class="mb-2 text-2xl font-bold tracking-tight">
+                                ${item.title}
+                              </h5>
+                            </a>
+                            <p
+                              class="mb-3 py-2 font-normal text-gray-600 border-b-2 border-gray-300 border-dotted"
+                            >
+                              ${item.description}
+                            </p>
 
-                <div class="flex flex-1 justify-between">
-                  <div class="flex gap-4 text-gray-500 items-center">
-                    <div class="">
-                      <i class="fa-regular fa-message"></i><span> ${item.comment_count}</span>
-                    </div>
-                    <div class="">
-                      <i class="fa-regular fa-eye"></i><span> ${item.view_count}</span>
-                    </div>
-                    <div class="">
-                      <i class="fa-regular fa-clock"></i><span> ${item.posted_time}</span>min
-                    </div>
-                  </div>
-                  <a
+                            <div class="flex flex-1 justify-between">
+                              <div class="flex gap-4 text-gray-500 items-center">
+                                <div class="">
+                                  <i class="fa-regular fa-message"></i><span> ${item.comment_count}</span>
+                                </div>
+                                <div class="">
+                                  <i class="fa-regular fa-eye"></i><span> ${item.view_count}</span>
+                                </div>
+                                <div class="">
+                                  <i class="fa-regular fa-clock"></i><span> ${item.posted_time}</span>min
+                                </div>
+                              </div>
+                              <a
 
-                    onclick="readCount()"
-                    class="read-btn bg-green-600 text-white px-4 py-2 m-2 rounded-full"
-                    ><i class="fa-solid fa-envelope-open"></i
-                  ></a>
-                </div>
-              </div>
-            </div>`;
+                                onclick="readCount()"
+                                class="read-btn bg-green-600 text-white px-4 py-2 m-2 rounded-full"
+                                ><i class="fa-solid fa-envelope-open"></i
+                              ></a>
+                            </div>
+                          </div>
+                        </div>`;
     allCardContainer.appendChild(newCard);
   });
   const readBtn = document.querySelectorAll(".read-btn");
   for (const btn of readBtn) {
     btn.addEventListener("click", function () {
-      count = count + 1;
       const addCount = document.getElementById("add-read-count");
       const read = document.getElementById("count");
       const div = document.createElement("div");
-      div.innerHTML = `
-            <div class="flex flex-col gap-3 my-4">
-                <div class="bg-white rounded-lg flex justify-between p-2">
-                    <h4>${data.posts.title}</h4>
-                    <p><i class="fa-regular fa-eye"></i><span>${data.posts.view_count}</span></p>
-                </div>
-            </div>`;
+      data.posts.forEach((item) => {
+        div.innerHTML = `<div class="flex flex-col gap-3 my-4 shadow-md">
+            <div class="bg-white rounded-lg flex justify-between p-2">
+              <h4 class=" font-bold ">${item.title}</h4>
+              <p><i class="fa-regular fa-eye"></i><span> ${item.view_count}</span></p>
+            </div>
+          </div>`;
+      });
+
+      count = count + 1;
       read.innerText = count;
       addCount.appendChild(div);
     });
   }
 };
 
-loadData();
-
-// -----------------------------------------------latestData function-------------------------------------------------------
+// -----------------------------------------------latestData section function-----------------------------------------
 
 const latestData = async () => {
   const res = await fetch(
@@ -114,7 +112,7 @@ const latestData = async () => {
       designation = `Unknown`;
     }
     newCard.innerHTML = `<div
-            class="max-w-sm bg-white border border-slate-300 rounded-xl shadow p-4"
+            class="max-w-sm bg-white border border-slate-300 rounded-xl p-4 shadow-lg"
           >
             <a href="#" class="w-40 h-40">
               <img
@@ -157,4 +155,5 @@ const latestData = async () => {
   });
 };
 
+loadData();
 latestData();
